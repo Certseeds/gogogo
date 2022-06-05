ced5d4a25162129804d7bb9835d7470c1c269153
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-05 09:26:58 +0000

    feature: update with log.Fatal
    </subject>
    
    Branch: master
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- greetings/gerrtings.go
+++ greetings/gerrtings.go
@@ -5,7 +5,7 @@ import "errors"
 
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("Input can not be empty")
+		return "", errors.New("input can not be empty")
 	}
 	message := fmt.Sprintf("Hello? %s Thank you! Thank you very much", name)
 	return message, nil
--- hello/hello.go
+++ hello/hello.go
@@ -5,16 +5,20 @@ package main
 import (
 	"fmt"
 	"gogogo/greetings"
+	"log"
 	"rsc.io/quote/v3"
 )
 
 func main() {
 	fmt.Printf("Hello,%s!\n", "world")
 	fmt.Println(quote.GoV3()) // update it to v3 is a stupid idea
-	world, err := greetings.Hello("world")
-	if err == nil {
-		fmt.Println(world)
-	} else {
+	world, err := greetings.Hello("World")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	if err != nil {
 		fmt.Println(err.Error())
+		log.Fatalln(err.Error()) // log.Fatal seems exit(-1)
+	} else {
+		fmt.Println(world)
 	}
 }
