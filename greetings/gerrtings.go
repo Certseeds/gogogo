0c9e8bc6dfc6f34952c9972b87fb3043c857e47a
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-05 09:46:46 +0000

    feature: add random && init for gerrtings.go
    </subject>
    
    Branch: master
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- greetings/gerrtings.go
+++ greetings/gerrtings.go
@@ -1,12 +1,34 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"runtime"
+	"time"
+)
 import "errors"
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("input can not be empty")
 	}
-	message := fmt.Sprintf("Hello? %s Thank you! Thank you very much", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	_, file, _, _ := runtime.Caller(0)
+	log.Printf("init %v finish \n", file)
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hello? %v Mi Fans",
+		"Do You Like Mi For %v?",
+		"Do You Like %v Band?",
+		"%v thankyou very much",
+	}
+	return formats[rand.Intn(len(formats))]
 }
