d7a4ef590ecc6302f7810e295b37470add9d6226
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-05 12:03:56 +0000

    feature: update go test
    </subject>
    
    Branch: master
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ greetings/greetings_test.go
@@ -0,0 +1,40 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloNameNotNull(t *testing.T) {
+	name := "liming"
+
+	message, err := Hello(name)
+
+	if err != nil {
+		t.Log("not empty input should not throw error")
+		t.Fail()
+	}
+	if len(message) == 0 {
+		t.Log("not empty input should return not-nil value")
+		t.Fail()
+	}
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	if !want.MatchString(message) {
+		t.Fatalf("Input Value should Return")
+	}
+}
+
+func TestHelloNameEmpty(t *testing.T) {
+	name := ""
+
+	message, err := Hello(name)
+
+	if err == nil {
+		t.Log("empty input should throw error")
+		t.Fail()
+	}
+	if len(message) != 0 {
+		t.Log("empty input should return empty value")
+		t.Fail()
+	}
+}
